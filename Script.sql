d419d0791b189f1df8d113b4ea3e3f3f3127e01d
Rahuldj <rj712@snu.edu.in>
2023-10-16 16:18:10 +0530

    Rahul-Commit

--- Script.sql
+++ Script.sql
@@ -1,14 +1,17 @@
 -- Path: Script.sql
-
 DROP DATABASE IF EXISTS squadron;
 CREATE DATABASE squadron;
 USE squadron;
-
+Create table faculty_Rahul(name varchar(30), rollNum int);
+insert into faculty_Rahul
+values("Rahul", 21101);
+select *
+from faculty_Rahul;
+desc faculty_Rahul;
 -- DROP TABLE IF EXISTS squadron.faculty;
 -- DROP TABLE IF EXISTS squadron.department;
 -- DROP TABLE IF EXISTS squadron.student;
 -- DROP TABLE IF EXISTS squadron.courses;
-
 -- CREATE TABLE squadron.faculty (
 --     faculty_id INT NOT NULL AUTO_INCREMENT,
 --     faculty_name VARCHAR(50) NOT NULL,
@@ -16,13 +19,11 @@ USE squadron;
 --     faculty_phone VARCHAR(50) NOT NULL,
 --     PRIMARY KEY (faculty_id)
 -- );
-
 -- CREATE TABLE squadron.department (
 --     department_id INT NOT NULL AUTO_INCREMENT,
 --     department_name VARCHAR(50) NOT NULL,
 --     PRIMARY KEY (department_id)
 -- );
-
 -- CREATE TABLE squadron.courses (
 --     course_id INT NOT NULL AUTO_INCREMENT,
 --     course_name VARCHAR(50) NOT NULL,
@@ -30,7 +31,5 @@ USE squadron;
 --     course_description VARCHAR(50) NOT NULL,
 --     PRIMARY KEY (course_id)
 -- );
-
 -- CREATE TABLE squadron.student (
-
 -- )
