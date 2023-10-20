d6cd81287c5d93f94130f3fa2cf28f536dc8a235
Mitaali Singhal <mitaalisinghal2811@gmail.com>
2023-10-20 10:38:43 +0530

    Mitaali commit

---
+++ squadron-next/src/pages/ComCourse.js
@@ -0,0 +1,17 @@
+import styles from '@/styles/Com.module.css'
+
+function ComCourse(){
+    return(
+        <>
+            <div className={styles.dabba1}>
+                <h1>ComCourse</h1>
+            </div>
+            <div>
+
+            </div>
+        </>
+        
+    )
+}
+
+export default ComCourse;
---
+++ squadron-next/src/pages/DesignCourse.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/Des.module.css'
+
+const DesignCourse=()=>{
+    return(
+        <div className={styles.dabba2}>
+            <h1>Design Course</h1>
+        </div>
+    )
+}
+export default DesignCourse;
