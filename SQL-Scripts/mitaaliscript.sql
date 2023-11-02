6629cf38db5e19202daedd90b605d86620452b74
Mitaali Singhal <mitaalisinghal2811@gmail.com>
2023-11-02 20:42:50 +0530

    Added courses

---
+++ SQL-Scripts/mitaaliscript.sql
@@ -0,0 +1,502 @@
+use squadron;
+
+call insertIntoCourse(
+    "DES102",
+    "Elements and Principles of Visual Design",
+    "The course introduces elements and principles of visual language through a series of
+lectures and studio sessions. ",
+    4,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "DES103",
+    "Drawing and Rendering",
+    "This practical course introduces students to the art of drawing and rendering in design.
+Through hands-on exercises, demonstrations, and projects, students will learn techniques to
+express ideas visually, communicate design concepts, and enhance their presentation skills. ",
+    2,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+
+call insertIntoCourse(
+    "DES104",
+    " Society, Culture and Design",
+    "This course make student aware of how society, culture, and design are related to each other.
+Through theoretical exploration, case studies, and practical applications, students will gain a
+comprehensive understanding of how societal and cultural contexts shape design choices and
+how design, in turn, influences and reflects broader societal trends. ",
+    4,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "DES105",
+    " Materials and Methods",
+    "This course exposes the students to different materials for realising the tangible solutions and
+acquaints them with various techniques and processes that are used in prototyping. The students
+will learn to analyse, select, and apply materials based on their properties and performance
+characteristics. ",
+    3,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+
+call insertIntoCourse(
+    "DES111",
+    " Introduction to Product Design",
+    "The course is intended to introduce the fundamentals of product design i.e.
+Involved methodology, different approaches as well as salient achievements in the field of product design. ",
+    4,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "DES131",
+    " Introduction To Ergonomics",
+    "The course is intended as an introduction to the domain of Ergonomics – the science of man – machine
+     interface and its importance to the Design. ",
+    4,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "DES201",
+    "Colour in Design",
+    "The course intends in developing understanding on the significance and applications of color in design. Students are expected to develop knowledge and practical skill through theoretical and practical training. 
+    Students are required to develop products, give seminars and submit research paper on chosen topics. ",
+    4,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "DES202",
+    " Information Structuring and Visualisation",
+    "The course intends in developing understanding on the information understanding and visualisation. ",
+    4,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "DES501",
+    " Introduction to Creative Design",
+    "The course intends in developing introduction to creative design ",
+    3,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "DES502",
+    " Elements and Principles of Design",
+    "The course intends in developing understanding of elements and Principles of design ",
+    4,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "DES503",
+    " Creative Design Morphology and Methods",
+    "The course intends in developing understanding of creative Design Morphology and Methods ",
+    4,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "DES504",
+    " Human Factors Engineering",
+    "The course intends in developing understanding of human Factors Engineering ",
+    4,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "DES505",
+    " Electronics and Software for Designers",
+    "The course intends in developing understanding of electronics and software for designers ",
+    2,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+
+call insertIntoCourse(
+    "DES506",
+    " Art and Craft Culture of India",
+    "The course intends in getting information on art and craft culture of India ",
+    3,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "DES507",
+    "  Design Semantics and Communication Theory",
+    "The course intends in getting information on design semantics and communication theory ",
+    3,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "DES508",
+    " Semester Research Project",
+    "The course focuses on a research project in semester ",
+    4,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+
+call insertIntoCourse(
+    "DES509",
+    " Technical Writing and Creative Communication",
+    "The course focuses on a research project in semester ",
+    2,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "DES510",
+    " Design Thinking",
+    "The course focuses on design thinking ",
+    2,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "DES511",
+    " Master's Thesis Project",
+    "This is first phase of the year-long Master’s Thesis Project that can be chosen by students
+based on their area of interest from any discipline/domains of Design as a field of study.  ",
+    4,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "DES512",
+    " Master's Thesis Project 2",
+    "This is second phase of the year-long Master’s Thesis Project that can be chosen by students
+based on their area of interest from any discipline/domains of Design as a field of study.  ",
+    8,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "DES521",
+    " Principles of Form",
+    "The course focuses on principles of form.",
+    4,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "DES522",
+    " Research Methods in Design",
+    "The course focuses on research methods in design.",
+    4,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "DES523",
+    " System Analysis and Information Design",
+    "The course focuses on system analysis and information Design.",
+    4,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "DES525",
+    " Design Management and Professional Practices",
+    "The course focuses on design management and Professional Practices",
+    4,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "DES526",
+    "Interaction Design",
+    "The course focuses on basic concepts in Interaction Design. Interaction Models – issues in man- machine
+interface, ergonomic considerations, dialog. Paradigms for interaction – time sharing,
+Video display units, Programming toolkits, Sensor based context aware interaction,",
+    4,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "DES527",
+    " Product Styling and Modelling",
+    "The course is intended as an
+introduction to the domain of Product styling in the Product Design.
+Content- The course intends to develop understanding about the minute detailing of a
+product related to its interior and exterior features (essential for a robust, better
+functioning, communicative, usable, visually appealing and easily manufacturable
+product)",
+    4,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "DES529",
+    " Visual Design",
+    "The course focuses on design management and Professional Practices",
+    4,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "DES531",
+    " Design for Sustainability",
+    "The course focuses on design for sustainability",
+    4,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "DES532",
+    " Information Systems, Architecture, and Cognitive Psychology",
+    "The course focuses on information Systems, architecture, and cognitive Psychology",
+    4,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "DES533",
+    " Design Virtual Reality",
+    "The course is a project-based course that includes assignments aligning with the
+theories to develop a virtual reality experience in practice.",
+    4,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "DES534",
+    "UX Research Methods",
+    "This course builds on DES522: Research Methods in Design and focuses on the contemporary
+     methods of UX Research.",
+    4,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+
+call insertIntoCourse(
+    "DES535",
+    "Sequential Narrative",
+    "The course aims to prepare students for visual communication through moving images or sequential
+communication. To equip students with a professional design approach for information
+visualization in a time-based medium.",
+    4,
+    "Offline",
+    "DES",
+    "Both",
+    1,
+    0
+);
+
+--Com courses
+
+call insertIntoCourse(
+    "COM191",
+    "Communicating the Environment",
+    "Communicating the Environment-Students in this class will learn how to contextualize, 
+    critically analyse and formulate narratives on a range of environmental issues. 
+    They will learn the skills and techniques they can apply to reporting on environmental issues
+     generally. And they will learn how to think about and explore the many ways in which the environment
+      intersects with nearly every aspect of our society, including health, psychology, architecture and urban design.",
+    4,
+    "Offline",
+    "COM",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "COM193",
+    "Sound Sense",
+    "This course will usher you into the enigmatic yet magnificent world of sound. Throughout the course our practice will be to thoughtfully consider what sound is and how we sense it and to investigate into the science behind auditory perception of human beings with an objective of knowing how sound interacts with 
+    the visual in audio-visual medium and how sound design conveys meaning.",
+    4,
+    "Offline",
+    "COM",
+    "Both",
+    1,
+    0
+);
+call insertIntoCourse(
+    "COM193",
+    "Sound Sense",
+    "This course will usher you into the enigmatic yet magnificent world of sound. Throughout the course our practice will be to thoughtfully consider what sound is and how we sense it and to investigate into the science behind auditory perception of human beings with an objective of knowing how sound interacts with 
+    the visual in audio-visual medium and how sound design conveys meaning.",
+    4,
+    "Offline",
+    "COM",
+    "Both",
+    1,
+    0
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+
+
+
